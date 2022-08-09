bfd009f44e83863c7c10c6640ccf509d49905074
Weekend-Dracon <102872724+Weekend-Dracon@users.noreply.github.com>
2022-08-09 22:20:50 +0500

    2

---
+++ js/main.js
@@ -0,0 +1,8 @@
+var selectinformation = movies.slice(0, 5);
+var elList = document.querySelector("#bootstrap__movies-list");
+var array = [];
+
+for (let q = 0; q < array.length; q++) {
+    const element = array[q];
+    
+}
